61d71713716fafca14da35f988ecf022628b2b8f
cameron-poehner <cameronpoehner@gmail.com>
2020-08-20 13:42:39 -0700

    added functionality and styling for marking complete and deleting todos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
-console.log('connected!');
+// Check off specific todos by clicking
+$('li').click(function() {
+    $(this).toggleClass('completed'); 
+});
+
+// Click on X to delete To-do
+
+$('span').click(function(e) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
