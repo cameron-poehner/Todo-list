549d306bdf6077bb2211781b7051b636fc1a84be
cameron-poehner <cameronpoehner@gmail.com>
2020-08-20 14:07:58 -0700

    added logic for creating todos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,22 @@
 // Check off specific todos by clicking
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed'); 
 });
 
 // Click on X to delete To-do
 
-$('span').click(function(e) {
+$('ul').on('click', 'span', function(e) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+    if (e.which === 13) {
+        // Grabbing a new todo from text input
+        var todoText = $(this).val();
+        // Create a new li and add to ul
+        $('ul').append(`<li>${todoText}</li>`);
+    }
 });
